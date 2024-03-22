b759a68efc792a36ca132515659a917f4e9beb04
yongteck <160562805+yongteck@users.noreply.github.com>
2024-03-22 08:22:11 +0000

    final submission

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 stdform = input('Enter a number in scientific notation: ')
 stdform = stdform.strip()
-
 # Type your code below
+stdpos = stdform.find("x")
+caretpos = stdform.find("^")
+mantissa = stdform[0:stdpos]
+expo = stdform[caretpos+1:]
+
+print(f"This number in E notation is {mantissa}E{expo}.")
